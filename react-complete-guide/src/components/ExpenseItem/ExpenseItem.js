7a78d0aa21ab49f17cb45dc3fdf96514a93db03b
Alex Lane <alex.lane@quadpay.com>
2021-11-12 20:41:32 -0500

    Dynamically create expenses

--- react-complete-guide/src/components/ExpenseItem/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,13 @@
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem({ title, amount, date }) {
   return (
     <div className="expense-item">
-      <div>March 28th, 2021</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.97</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
