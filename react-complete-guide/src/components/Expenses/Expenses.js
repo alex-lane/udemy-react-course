a2ab563d464a82e0bd706465b4720d7d2871cd04
Alex Lane <alex.lane@quadpay.com>
2021-11-16 11:52:27 -0500

    Create expenses list as a separate component

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Card from "../UI/Card/Card";
 import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
-import ExpenseItem from "./ExpenseItem/ExpenseItem";
+import ExpensesList from "./ExpensesList/ExpensesList";
 import "./Expenses.css";
 
 const Expenses = ({ expenses }) => {
@@ -16,26 +16,13 @@ const Expenses = ({ expenses }) => {
     return expense.date.getFullYear().toString() === year;
   });
 
-  let expensesContent = <p>No expenses found</p>;
-
-  if (filteredExpensesByYear.length > 0) {
-    expensesContent = filteredExpensesByYear.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={year}
         onYearChange={onFilterYearChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList expenses={filteredExpensesByYear} />
     </Card>
   );
 };
