eba9d5e4b0962e55b8e0abad950c2eb2dd6df95c
Alex Lane <alex.lane@quadpay.com>
2021-11-16 10:11:03 -0500

    Create filter

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 import Card from "../UI/Card/Card";
+import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 import ExpenseItem from "./ExpenseItem/ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = ({ expenses }) => {
+  const [year, setYear] = useState("2020");
+
+  const onFilterYearChangeHandler = (selectedYear) => {
+    console.log("In Expenses.js selected: " + selectedYear);
+    setYear(selectedYear);
+    console.log("In Expenses.js state: " + year);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selectedYear={year}
+        onYearChange={onFilterYearChangeHandler}
+      />
       {expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
