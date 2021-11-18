5d03dd3fb9d1bd02c6cb42d00a8aa5af117936a6
Alex Lane <alex.lane@quadpay.com>
2021-11-18 14:52:05 -0500

    Create button component

--- add-user-project/src/components/Users/AddUser/AddUser.js
+++ add-user-project/src/components/Users/AddUser/AddUser.js
@@ -1,21 +1,20 @@
 import { v4 as uuid4 } from "uuid";
-import Card from "../../Card/Card";
+import Card from "../../UI/Card/Card";
 import Input from "../../UI/Input/Input";
-import styles from "./AddUser.module.css";
+import Button from "../../UI/Button/Button";
 
 const AddUser = ({ onAddUser }) => {
   const addUserHandler = (event) => {
     event.preventDefault();
-
     onAddUser({ id: uuid4(), name: "Test", age: 25 });
   };
 
   return (
     <Card>
-      <form className={styles["add-user-form"]} onSubmit={addUserHandler}>
+      <form onSubmit={addUserHandler}>
         <Input type="text" id="username" displayName="Username" />
         <Input type="number" id="age" displayName="Age (Years)" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
