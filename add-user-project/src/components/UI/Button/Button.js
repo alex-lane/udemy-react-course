8e45b7aeaf532b8104e3c4754c947bca7850c2f6
Alex Lane <alex.lane@quadpay.com>
2021-11-18 15:05:30 -0500

    Dynamically apply additional classes to UI components

--- add-user-project/src/components/UI/Button/Button.js
+++ add-user-project/src/components/UI/Button/Button.js
@@ -1,8 +1,12 @@
 import styles from "./Button.module.css";
 
-const Button = ({ type, onClick, children }) => {
+const Button = ({ type, onClick, className, children }) => {
   return (
-    <button type={type} className={styles.button} onClick={onClick}>
+    <button
+      type={type}
+      className={`${styles.button} ${className}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
--- add-user-project/src/components/UI/Input/Input.js
+++ add-user-project/src/components/UI/Input/Input.js
@@ -1,8 +1,8 @@
 import styles from "./Input.module.css";
 
-const Input = ({ id, type, displayName }) => {
+const Input = ({ id, type, className, displayName }) => {
   return (
-    <div className={styles["form-control"]}>
+    <div className={`${styles["form-control"]} ${className}`}>
       <label htmlFor={id}>{displayName}</label>
       <input id={id} type={type} />
     </div>
