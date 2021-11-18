4870b7a264cd5153867594472fd52eba32e94e2d
Alex Lane <alex.lane@quadpay.com>
2021-11-18 16:33:18 -0500

    Show error modal

---
+++ add-user-project/src/components/UI/ErrorModal/ErrorModal.js
@@ -0,0 +1,24 @@
+import Button from "../Button/Button";
+import Card from "../Card/Card";
+import styles from "./ErrorModal.module.css";
+
+const ErrorModal = ({ title, message, onCloseClick }) => {
+  return (
+    <>
+      <div className={styles.backdrop} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={onCloseClick}>Close</Button>
+        </footer>
+      </Card>
+    </>
+  );
+};
+
+export default ErrorModal;
--- add-user-project/src/components/Users/AddUser/AddUser.js
+++ add-user-project/src/components/Users/AddUser/AddUser.js
@@ -3,12 +3,15 @@ import { v4 as uuid4 } from "uuid";
 import Card from "../../UI/Card/Card";
 import Input from "../../UI/Input/Input";
 import Button from "../../UI/Button/Button";
+import ErrorModal from "../../UI/ErrorModal/ErrorModal";
 
 const AddUser = ({ onAddUser }) => {
   const [userInput, setUserInput] = useState({
     username: "",
     age: "",
   });
+
+  const [error, setError] = useState();
 
   const usernameChangeHandler = (event) => {
     setUserInput((prevState) => {
@@ -29,10 +32,18 @@ const AddUser = ({ onAddUser }) => {
       userInput.username.trim().length === 0 ||
       userInput.age.trim().length === 0
     ) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid user name and age (non-empty values).",
+      });
       return;
     }
 
     if (+userInput.age < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
 
@@ -44,25 +55,34 @@ const AddUser = ({ onAddUser }) => {
   };
 
   return (
-    <Card>
-      <form onSubmit={addUserHandler}>
-        <Input
-          type="text"
-          id="username"
-          value={userInput.username}
-          displayName="Username"
-          onChange={usernameChangeHandler}
+    <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onCloseClick={() => setError()}
         />
-        <Input
-          type="number"
-          id="age"
-          value={userInput.age}
-          displayName="Age (Years)"
-          onChange={ageChangeHandler}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card>
+        <form onSubmit={addUserHandler}>
+          <Input
+            type="text"
+            id="username"
+            value={userInput.username}
+            displayName="Username"
+            onChange={usernameChangeHandler}
+          />
+          <Input
+            type="number"
+            id="age"
+            value={userInput.age}
+            displayName="Age (Years)"
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </>
   );
 };
 
