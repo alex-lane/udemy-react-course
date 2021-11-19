29a2a703509e8ab7be1fd915061c74d5fa74f701
Alex Lane <alex.lane@quadpay.com>
2021-11-19 11:57:35 -0500

    create portal for error modal

--- add-user-project/src/components/UI/ErrorModal/ErrorModal.js
+++ add-user-project/src/components/UI/ErrorModal/ErrorModal.js
@@ -1,22 +1,44 @@
+import ReactDOM from "react-dom";
+
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import styles from "./ErrorModal.module.css";
 
+const Backdrop = ({ onCloseClick }) => {
+  return <div className={styles.backdrop} onClick={onCloseClick} />;
+};
+
+const ModalOverlay = ({ title, message, onCloseClick }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onCloseClick}>Close</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = ({ title, message, onCloseClick }) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={onCloseClick} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onCloseClick}>Close</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseClick={onCloseClick} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={title}
+          message={message}
+          onCloseClick={onCloseClick}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
